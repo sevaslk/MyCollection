c6f121cbef2ab64d174265aa6b1334bceb0117b9
Vyacheslav Kirizleev <slava91274@gmail.com>
2020-07-03 20:54:13 +0300

    findDuplicatesUsingHashSet() added

--- src/com/task3/Task3.java
+++ src/com/task3/Task3.java
@@ -1,13 +1,15 @@
 package com.task3;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Task3 {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 4, 5, 5, 6, 7, 7, 8};
+        int[] arr = new int[]{11, 2, 35, 4, 5, 5, 6, 7, 7, 8};
         findDuplicates(arr);
+        findDuplicatesUsingHashSet(arr);
     }
 
     private static void findDuplicates(int[] inputArray) {
@@ -23,6 +25,15 @@ public class Task3 {
         for (Map.Entry<Integer, Integer> entry : entrySet) {
             if (entry.getValue() > 1) {
                 System.out.println("Duplicate Element : " + entry.getKey() + " - found " + entry.getValue() + " times.");
+            }
+        }
+    }
+
+    private static void findDuplicatesUsingHashSet(int[] inputArray) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int element : inputArray) {
+            if (!set.add(element)) {
+                System.out.println("Duplicate Element : " + element);
             }
         }
     }
