40d3f2cb9dbf5b85450f78289a1ca311bcaacef6
Vyacheslav Kirizleev <slava91274@gmail.com>
2020-07-04 12:18:06 +0300

    task2: type of return changed, task3: left only checking for duplicates(boolean)

--- src/com/task2/Task2.java
+++ src/com/task2/Task2.java
@@ -8,40 +8,40 @@ public class Task2 {
         Integer[] arr2 = {5, 2, 1, 7, 4, 6, 3, 8};
         List<Integer> arrayList = new ArrayList<>(Arrays.asList(arr2));
 
-        arrayIndexSumCheck(arr, 5);
-        arrayIndexSumCheckWithList(arrayList, 15);
+        System.out.println(Arrays.toString(arrayIndexSumCheck(arr, 5)));
+        System.out.println(arrayIndexSumCheckWithList(arrayList, 15));
 
     }
 
-    public static boolean arrayIndexSumCheck(int[] array, int value) {
+    static int[] arrayIndexSumCheck(int[] array, int value) {
         Arrays.sort(array);
         int start = 0;
         int end = array.length - 1;
+        int[] resultArray = new int[2];
         while (start < end) {
             if ((array[start] + array[end]) > value) {
                 end--;
             } else if ((array[start] + array[end]) < value) {
                 start++;
             } else {
-                System.out.println(start + ", " + end);
-                return true;
+                resultArray = new int[]{start, end};
+                return resultArray;
             }
         }
-        return false;
+        return resultArray;
     }
 
-    public static boolean arrayIndexSumCheckWithList(List<Integer> list, int value) {
+    static Set<Integer> arrayIndexSumCheckWithList(List<Integer> list, int value) {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < list.size(); i++) {
             int arg2 = value - list.get(i);
             if (list.contains(arg2)) {
                 set.add(list.indexOf(list.get(i)));
                 set.add(list.indexOf(arg2));
-                System.out.println(set);
-                return true;
+                return set;
             }
         }
-        return false;
+        return set;
     }
 
 }
--- src/com/task3/Task3.java
+++ src/com/task3/Task3.java
@@ -2,40 +2,34 @@ package com.task3;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class Task3 {
     public static void main(String[] args) {
         int[] arr = new int[]{11, 2, 35, 4, 5, 5, 6, 7, 7, 8};
-        findDuplicates(arr);
-        findDuplicatesUsingHashSet(arr);
+        System.out.println(findDuplicatesUsingHashMap(arr));
+        System.out.println(findDuplicatesUsingHashSet(arr));
     }
 
-    private static void findDuplicates(int[] inputArray) {
+    private static boolean findDuplicatesUsingHashMap(int[] inputArray) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int element : inputArray) {
             if (map.get(element) == null) {
                 map.put(element, 1);
             } else {
-                map.put(element, map.get(element) + 1);
+                return true;
             }
         }
-        Set<Map.Entry<Integer, Integer>> entrySet = map.entrySet();
-        for (Map.Entry<Integer, Integer> entry : entrySet) {
-            if (entry.getValue() > 1) {
-                System.out.println("Duplicate Element : " + entry.getKey() + " - found " + entry.getValue() + " times.");
-            }
-        }
+        return false;
     }
 
-    private static void findDuplicatesUsingHashSet(int[] inputArray) {
-        HashSet<Integer> set = new HashSet<Integer>();
+    private static boolean findDuplicatesUsingHashSet(int[] inputArray) {
+        HashSet<Integer> set = new HashSet<>();
         for (int element : inputArray) {
             if (!set.add(element)) {
-                System.out.println("Duplicate Element : " + element);
+                return true;
             }
         }
+        return false;
     }
 }
 
